9e3a0fbb99a10e3d1008fbe03447ee6c669924ec
Sergio <sage_sergio@hotmail.com>
2025-07-17 13:40:36 -0600

    Apis para edición de workspaces y context

--- fileuploads/urls.py
+++ fileuploads/urls.py
@@ -9,9 +9,11 @@ urlpatterns = [
     # apis para workspaces admin
     path('workspaces/admin', views.list_admin_workspaces, name='admin-workspace-list'),
     path('workspaces/admin/create', views.create_admin_workspaces, name='admin-workspace-create'),
+    path('workspaces/admin/edit/<int:workspace_id>', views.edit_admin_workspaces, name='admin-workspace-edit'),
     
     path('workspaces/admin/<int:workspace_id>/contexts', views.list_admin_workspaces_contexts, name='admin-contexts-list'),   
     path('workspaces/admin/contexts/create', views.create_admin_workspaces_contexts, name='contexts-create'),
+    path('workspaces/admin/contexts/edit/<int:context_id>', views.edit_admin_workspaces_contexts, name='admin-contexts-edit'),
     
     path('workspaces/admin/<int:workspace_id>/contexts/<int:context_id>/files', views.list_admin_workspaces_contexts_files, name='admin-contexts-files-list'),   
     path('workspaces/admin/contexts/files/create', views.create_admin_workspaces_contexts_files, name='contexts-files-create'),   
--- fileuploads/views.py
+++ fileuploads/views.py
@@ -101,6 +101,66 @@ def create_admin_workspaces(request):
         return JsonResponse({"status": "error", "message": str(e)}, status=400)
 
 
+@api_view(["GET", "POST"])
+@csrf_exempt
+def edit_admin_workspaces(request, workspace_id):
+    user_id = request.GET.get("user_id")
+    workspace_data = request.POST.copy()
+    answer = {
+        "id": None,
+        "saved": False
+    }
+    
+    file = request.FILES.get('file', None)
+    if file:
+        filename = file.name
+        file_extension = filename.split('.')[-1].lower()
+        
+        if file_extension not in ['jpg', 'jpeg', 'png']:
+            return JsonResponse(
+                {"error": "El archivo debe ser una imagen (jpg, jpeg, png)"},
+                status=400
+            )
+    
+    try:
+        with transaction.atomic():
+            
+            get_workspace               = Workspace.objects.get(id=workspace_id)
+            get_workspace.user_id       = user_id
+            get_workspace.title         = workspace_data.get("title")
+            get_workspace.description   = workspace_data.get("description")
+            get_workspace.public        = workspace_data.get("public")
+            
+            if file:   
+                get_workspace.image_type = file_extension
+                
+            get_workspace.save()
+            
+            if file:   
+                try:
+                    # Crear el directorio si no existe
+                    upload_dir = "uploaded_images"
+                    if not os.path.exists(upload_dir):
+                        os.makedirs(upload_dir)
+
+                    workspaces_dir = os.path.join(upload_dir, "workspaces")
+                    if not os.path.exists(workspaces_dir):
+                        os.makedirs(workspaces_dir)
+                    
+                    # Guardar la imagen en el directorio
+                    file_path = os.path.join(workspaces_dir, '{}.{}'.format(workspace_id, file_extension))
+                    with open(file_path, "wb") as f:
+                        shutil.copyfileobj(file.file, f)
+                except:
+                    l = 0
+                    
+            answer["id"] = get_workspace.id
+            answer["saved"] = True
+        return JsonResponse(answer, status=200)
+    except Exception as e:
+        return JsonResponse({"status": "error", "message": str(e)}, status=400)
+
+
 
 """
     Secciones de apis para contexts
@@ -191,6 +251,65 @@ def create_admin_workspaces_contexts(request):
                     l = 0
                     
             answer["id"] = new_context.id
+            answer["saved"] = True
+        
+        return JsonResponse(answer, status=200)    
+    except Exception as e:
+        return JsonResponse({"status": "error", "message": str(e)}, status=400)
+
+@api_view(["GET", "POST"])
+def edit_admin_workspaces_contexts(request, context_id):
+    user_id = request.GET.get("user_id")
+    context_data = request.POST.copy()
+    
+    answer = {
+        "id": None,
+        "saved": False
+    }
+    
+    file = request.FILES.get('file', None)
+    if file:
+        filename = file.name
+        file_extension = filename.split('.')[-1].lower()
+        
+        if file_extension not in ['jpg', 'jpeg', 'png']:
+            return JsonResponse(
+                {"error": "El archivo debe ser una imagen (jpg, jpeg, png)"},
+                status=400
+            )
+    
+    try:
+        with transaction.atomic():
+            get_context               = Context.objects.get(id=context_id)
+            get_context.user_id       = user_id
+            get_context.title         = context_data.get("title")
+            get_context.description   = context_data.get("description")
+            get_context.public        = context_data.get("public")
+            
+            if file:   
+                get_context.image_type = file_extension
+                
+            get_context.save()
+            
+            if file:   
+                try:
+                    # Crear el directorio si no existe
+                    upload_dir = "uploaded_images"
+                    if not os.path.exists(upload_dir):
+                        os.makedirs(upload_dir)
+
+                    contexts_dir = os.path.join(upload_dir, "contexts")
+                    if not os.path.exists(contexts_dir):
+                        os.makedirs(contexts_dir)
+                    
+                    # Guardar la imagen en el directorio
+                    file_path = os.path.join(contexts_dir, '{}.{}'.format(context_id, file_extension))
+                    with open(file_path, "wb") as f:
+                        shutil.copyfileobj(file.file, f)
+                except:
+                    l = 0
+                    
+            answer["id"] = get_context.id
             answer["saved"] = True
         
         return JsonResponse(answer, status=200)    
